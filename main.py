332da363a85f35177cdaccc28ba989b46df09cb8
augustin <augustin7766@gmail.com>
2024-02-18 11:54:16 +0530

    Data_validation

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from logger_file.logging import logger
-from src.IrisFlowerClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.IrisFlowerClassification.pipeline.stage_01_data_ingestion import (DataIngestionTrainingPipeline)
+from src.IrisFlowerClassification.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from Exception_file.exception import CustomException
 import sys
-
+from Exception_file.exception import CustomException
 
 
 
@@ -19,3 +20,16 @@ try:
 
 except Exception as e:
     raise CustomException(e, sys)
+
+
+
+STAGE_NAME = 'Data_Validation_Stage'
+
+
+try:
+    logger.info(f'---------------{STAGE_NAME} started-------------------')
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f'------------------{STAGE_NAME} completed----------------')
+except Exception as e:
+    raise CustomException(e, sys) 
---
+++ src/IrisFlowerClassification/pipeline/stage_02_data_validation.py
@@ -0,0 +1,24 @@
+
+from src.IrisFlowerClassification.config.configuration import ConfigurationManager
+from src.IrisFlowerClassification.components.data_validation import DataValidation
+
+
+
+
+
+
+
+
+STAGE_NAME = 'Data_Validation_Stage'
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config= data_validation_config)
+        data_validation.validate_all_columns()
+
+
